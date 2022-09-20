d1532a0bf8a0a9b562c93503b83f1744cbfe8fd7
Mohit Dhatrak <91209576+mohitdhatrak@users.noreply.github.com>
2022-09-20 15:56:11 +0000

    completed task 1 logic

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
-var readlineSync = require("readline-sync");
+// task 1 - Logic
+
+/* You are given n words. Some words may repeat. For each word, find its number of occurrences. The output should contain the number of occurrences of each distinct word. See the sample input/output for clarification. */
+
+const readlineSync = require("readline-sync");
+
+console.log("Input Format: The first line of the input is n. The next n lines each contain a word. (Make sure you enter each word in a new line) \n");
+console.log(
+  `Sample Input:
+4
+bcdef
+abcdefg
+bcde
+bcdef \n`);
+
+const wordsArr = [];
+
+// taking user input
+const numOfWords = readlineSync.question("Enter your input: \n");
+for (let i = 0; i < numOfWords; i++) {
+  wordsArr[i] = readlineSync.question();
+}
+
+const wordsObj = {};
+
+const isRepeating = (word) => {
+  if (wordsObj[word] === undefined) {
+    wordsObj[word] = 1;
+  }
+  else {
+    wordsObj[word]++;
+  }
+}
+
+wordsArr.forEach(word => isRepeating(word));
+
+// array having number of repetitions of each unique word (order of words same as order of input words)
+const numOfRepetitions = Object.values(wordsObj);
+
+/* Output Format: On the first line, the output should be the number of distinct words from the input. On the second line, print the number of occurrences for each distinct word according to their appearance in the input. */
+console.log("\nOutput:");
+console.log(numOfRepetitions.length);
+console.log(numOfRepetitions.join(" "));
+
